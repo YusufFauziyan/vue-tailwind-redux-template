8b8867fcf9714f14fed8665f2ce71e7b8169b0d3
yusuffauziyan <yusuffauziyan@gmail.com>
2023-05-10 14:30:24 +0700

    commit

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useCounterStore } from "../app/store"
+
+const {count} = useCounterStore()
+</script>
+
+
+<template>
+	<main id="about-page">
+		<h1>About</h1>
+		<p>This is the about page</p>
+		<p>From Redux</p>
+		<p>{{ count }}</p>
+	</main>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useCounterStore } from "../app/store"
+
+const store = useCounterStore()
+
+</script>
+
+<template>
+	<main id="Home-page">
+		<h1>Home</h1>
+		<p>{{ store.count }}</p>
+		<div class="flex gap-4 items-center">
+			<button @click="store.increment">+</button>
+			<button @click="store.decrement">-</button>
+		</div>
+	</main>
+</template>
